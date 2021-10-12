49f597319ad993bc9dbcd4e109682b5608af4833
kynel <kynel95@gmail.com>
2021-10-12 16:14:46 +0900

    implement mutation(add, delete) with movies data

--- graphql/db.js
+++ graphql/db.js
@@ -1,31 +1,49 @@
-export const people = [
+let movies = [
   {
     id: 0,
-    name: 'kynel',
-    age: 27,
-    gender: 'male',
+    name: 'Star Wars - The new one',
+    score: 1,
   },
   {
     id: 1,
-    name: 'june',
-    age: 27,
-    gender: 'male',
+    name: 'Avengers - the new one',
+    score: 8,
   },
   {
     id: 2,
-    name: 'link',
-    age: 27,
-    gender: 'male',
+    name: 'The Godfather I',
+    score: 99,
   },
   {
     id: 3,
-    name: 'mario',
-    age: 27,
-    gender: 'male',
+    name: 'Logan',
+    score: 2,
   },
 ];
 
+export const getMovies = () => movies;
+
 export const getById = (id) => {
-  const filteredPeople = people.filter((person) => id === person.id);
-  return filteredPeople[0];
+  const filteredMovies = movies.filter((movie) => id === movie.id);
+  return filteredMovies[0];
+};
+
+export const deleteMovie = (id) => {
+  const cleanedMovies = movies.filter((movie) => movie.id !== id);
+  if (movies.length > cleanedMovies.length) {
+    movies = cleanedMovies;
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export const addMovie = (name, score) => {
+  const newMovie = {
+    id: `${movies.length + 1}`,
+    name,
+    score,
+  };
+  movies.push(newMovie);
+  return newMovie;
 };
